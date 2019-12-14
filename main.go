3ea5ab37a99852598801358031800d3300efd7a2
Maartje Eyskens <maartje@eyskens.me>
2019-12-14 10:37:10 +0100

    Fork from ABCBoard
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/zserge/lorca"
+
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+)
+
+var ui lorca.UI
+var controller = PanelController{Panels: []Panel{}, playCancelers: map[string]context.CancelFunc{}, playPausers: map[string]*sync.Mutex{}}
+
+// Panel is one panel to be shown
+type Panel struct {
+	Name     string `json:"name"`
+	Shortcut string `json:"shortcut"`
+	File     string `json:"file"`
+}
+
+// PanelController helps with controling the pannels
+type PanelController struct {
+	Panels        []Panel `json:"panels"`
+	panelsForFile map[string]*Panel
+	playCancelers map[string]context.CancelFunc
+	playPausers   map[string]*sync.Mutex
+}
+
+// GetFromDisk loads the panels from the config file
+func (p *PanelController) GetFromDisk() []Panel {
+	f, err := os.Open("./panels.json")
+	jsonParser := json.NewDecoder(f)
+	jsonParser.Decode(&p.Panels)
+	if err != nil {
+		fmt.Println(err)
+		return p.Panels
+	}
+
+	p.panelsForFile = map[string]*Panel{}
+	for i := range p.Panels {
+		p.panelsForFile[p.Panels[i].File] = &p.Panels[i]
+	}
+	return p.Panels
+}
+
+// Play starts playing a specific file
+func (p *PanelController) Play(file string) {
+	fmt.Println(file)
+	ctx, cancel := context.WithCancel(context.Background())
+	p.playCancelers[file] = cancel
+	pause := sync.Mutex{}
+	p.playPausers[file] = &pause
+	go func() {
+		playMP3(ctx, &pause, file)
+		cancel()
+		p.playCancelers[file] = nil
+		ui.Eval("window.eventEmitter.emit('endSound','" + file + "')")
+	}()
+}
+
+// Cancel stops playing a specific file
+func (p *PanelController) Cancel(file string) {
+	if cancel := p.playCancelers[file]; cancel != nil {
+		cancel()
+	}
+}
+
+// Pause pauses playing a specific file
+func (p *PanelController) Pause(file string) {
+	if mutex := p.playPausers[file]; mutex != nil {
+		mutex.Lock()
+	}
+}
+
+// Resume pauses playing a specific file
+func (p *PanelController) Resume(file string) {
+	if mutex := p.playPausers[file]; mutex != nil {
+		mutex.Unlock()
+	}
+}
+
+func handleAPIPanels(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+	out, _ := json.Marshal(controller.GetFromDisk())
+	w.Write(out)
+}
+
+func main() {
+	var err error
+	ui, err = lorca.New("", "", 480, 320)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go func() {
+		// load in bindata
+		http.Handle("/api/panels", http.HandlerFunc(handleAPIPanels))
+		http.Handle("/", http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "/frontend/build"}))
+		log.Fatal(http.Serve(ln, nil))
+	}()
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
+
+	log.Println("DOM bind")
+
+	ui.Bind("play", controller.Play)
+	ui.Bind("pause", controller.Pause)
+	ui.Bind("cancel", controller.Cancel)
+	ui.Bind("resume", controller.Resume)
+	ui.Eval(`
+			window.panelController = {
+				play,
+				pause,
+				cancel,
+				resume,
+			}
+		`)
+	log.Println(err)
+
+	// Wait for the browser window to be closed
+	<-ui.Done()
+}
---
+++ play.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"sync"
+
+	mp3 "github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
+)
+
+var cgoMutex = sync.Mutex{} // attempt to fix cgo breaking...
+
+func playMP3(ctx context.Context, pause *sync.Mutex, file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	cgoMutex.Lock()
+	p, err := oto.NewPlayer(d.SampleRate(), 2, 2, 8192)
+	cgoMutex.Unlock()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	doneCopy := make(chan bool)
+	writeMutex := sync.Mutex{}
+	go func() {
+		buf := make([]byte, 100)
+		for {
+			pause.Lock()
+			writeMutex.Lock()
+			n, err := d.Read(buf)
+			if err != nil {
+				break
+			}
+			p.Write(buf[:n])
+			writeMutex.Unlock()
+			pause.Unlock()
+		}
+		doneCopy <- true
+	}()
+L:
+	for {
+		select {
+		case <-doneCopy:
+			cgoMutex.Lock()
+			f.Close()
+			p.Close()
+			d.Close()
+			cgoMutex.Unlock()
+			break L
+		case <-ctx.Done():
+			writeMutex.Lock()
+			cgoMutex.Lock()
+			d.Close()
+			p.Close()
+			f.Close()
+			cgoMutex.Unlock()
+			writeMutex.Unlock()
+			break L
+		}
+	}
+}
